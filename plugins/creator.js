704e1dac07c7cf39884f9cb5e02aa8678aa1e74f
MartinOfc <hafizhkaka742@gmail.com>
2022-08-07 18:05:16 +0700

    creator.js

--- plugins/creator.js
+++ plugins/creator.js
@@ -21,17 +21,17 @@ let handler  = async (m, { conn, command, args, usedPrefix, DevMode }) => {
 //------------ BIO
 let ppown = await conn.profilePictureUrl(nomorown + '@s.whatsapp.net', 'image').catch(_ => hwaifu[1]) 
 let teksbio = `${htki} *BIODATA* ${htka}
-*💌 Nama* : Fangz
-*✉️ Nama RL* : FangzBOT
+*💌 Nama* : Martin
+*✉️ Nama BOT* : AHMAD BOT
 *♂️ Gender* : Laki - laki
 *🕋 Agama* : Islam
-*⏰ Tanggal lahir* : 25 oktober 2007
+*⏰ Tanggal lahir* : 25 September 2008
 *🎨 Umur* : 15
 *🧮 Kelas* : 9
-*🧩 Hobby* : Nonton henta*, main game, Recode script
-*💬 Sifat* : Baik, hode, softboy, tydack ramah, g*y
-*🗺️ Tinggal* : Indonesia, jawa, bogor
-*❤️ Suka* : warnah pink & hitam, trap, waifu wangy, kucing
+*🧩 Hobby* : Nonton Anime, main game, Recode script
+*💬 Sifat* : Baik, pinter
+*🗺️ Tinggal* : Indonesia, Lampung, Bandar lampung
+*❤️ Suka* : warnah pink & hitam,merah, biru
 *💔 Benci* : anak alay, autis, anak epep, sok asik
 
 *- - sᴋɪʟʟs: - -* 
@@ -47,9 +47,9 @@ let teksbio = `${htki} *BIODATA* ${htka}
 📷 *Instagram:* ${sig}
 🐈 *Github:* ${sgh}
 🥏 *Whatsapp* wa.me/${nomorown}
-🌐 *Discord:* Soon
-🌏 *Website:* https://fangzbot.websites.co.in/
-🎶 *Tiktok:* tiktok.com/@fangzjb
+🌐 *Discord:* Ga Ada Ngabs
+🌏 *Website:* Ga Ada Ngabs
+🎶 *Tiktok:*  Ga ada Ngabs
 
 `
   let teks = ' '
@@ -77,7 +77,7 @@ const listMessage = {
   text: teks,
   footer: null,
   title: `${htki} *OWNER* ${htka}`,
-  buttonText: "Click Here !",
+  buttonText: "Click !",
   sections
 }
 
